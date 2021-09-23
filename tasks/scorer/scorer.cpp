aecdbb3e1ad0b342feea1f93f92a560dad4d4af0
Ihor Chovpan <cigo1234r@gmail.com>
2021-09-23 20:42:13 +0300

    fixed the code style

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -7,11 +7,11 @@ using std::set;
 using std::sort;
 using std::string;
 using std::tie;
+
 struct StudentTask {
     string student_name;
     string task_name; 
-    bool operator<(const StudentTask& another) const
-    {
+    bool operator<(const StudentTask& another) const {
         return tie(student_name, task_name) < tie(another.student_name, another.task_name);
     }
 };
@@ -20,8 +20,7 @@ map<StudentTask, bool> last_check_success;
 map<StudentTask, int> opened_merge_requests;
 map<string, set<string> > tasks_solved_by_student;
 
-bool compare_by_time(const Event& event1, const Event &event2)
-{
+bool compare_by_time(const Event& event1, const Event &event2) {
     return (event1.time < event2.time);
 }
 
@@ -29,7 +28,7 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     Events sorted_events = events;
     sort(sorted_events.begin(), sorted_events.end(), compare_by_time);
     for (Event event : sorted_events) {
-        if (event.time>score_time)
+        if (event.time > score_time)
             break;
 
         StudentTask student_task = StudentTask{.student_name = event.student_name, .task_name = event.task_name};
@@ -40,15 +39,15 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
             case EventType::CheckFailed:
                 last_check_success[student_task] = false;
             case EventType::MergeRequestOpen:
-                opened_merge_requests[student_task]++;
+                ++opened_merge_requests[student_task];
             case EventType::MergeRequestClosed:
-                opened_merge_requests[student_task]--;
+                --opened_merge_requests[student_task];
             default:
                 break;
         }
     }
     for (auto [student_task, last_check] : last_check_success) {
-        if (last_check && opened_merge_requests[student_task]==0) 
+        if (last_check && opened_merge_requests[student_task] == 0) 
             tasks_solved_by_student[student_task.student_name].insert(student_task.task_name);
     }
     return tasks_solved_by_student;
