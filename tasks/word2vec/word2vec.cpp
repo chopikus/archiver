7891c85a4dacc97f54eaf3c7c2d9242d591e7ca5
Ihor Chovpan <cigo1234r@gmail.com>
2021-09-24 22:43:51 +0300

    solved task word2vec

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -1,8 +1,44 @@
+#include <cmath>
+#include <vector>
+#include <stdexcept>
 #include "word2vec.h"
 
-#include <vector>
+using std::string;
+using std::vector;
 
-std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
-                                          const std::vector<std::vector<int>>& vectors) {
-    return {};
+double ScalarOnNorm(const vector<double>& norm1, const vector<double>& norm2) {
+    double result = 0;
+    for (size_t i = 0; i < std::min(norm1.size(), norm2.size()); i++) {
+        result += norm1[i] * norm2[i];
+    }
+    return result;
+}
+
+vector<string> FindClosestWords(const vector<string>& words, const vector<vector<int>>& vectors) {
+    vector<vector<double>> normalized_vectors;
+    for (const vector<int>& v : vectors) {
+        double len = 0;
+        for (int x : v) {
+            len += x * x;
+        }
+        len = sqrt(len);
+        normalized_vectors.emplace_back(vector<double>());
+        for (int x : v) {
+            double normalized_x = x;
+            normalized_x /= len;
+            normalized_vectors.back().push_back(normalized_x);
+        }
+    }
+    double mx = 0;
+    for (size_t i = 1; i < normalized_vectors.size(); i++) {
+        mx = std::max(mx, ScalarOnNorm(normalized_vectors[0], normalized_vectors[i]));
+    }
+    vector<string> result;
+    for (size_t i = 1; i < normalized_vectors.size(); i++) {
+        double sc = ScalarOnNorm(normalized_vectors[0], normalized_vectors[i]);
+        if (sc == mx) {
+            result.push_back(words[i]);
+        }
+    }
+    return result;
 }
