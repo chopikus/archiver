a867f4de5dcdb1a8262192d062f7fe5da75990b6
Ihor Chovpan <cigo1234r@gmail.com>
2021-10-05 23:42:24 +0300

    parsing arguments

--- error_handler/error_handler.h
+++ error_handler/error_handler.h
@@ -1,12 +1,10 @@
-#pragma once
-
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 class ErrorHandler {
     public:
         static void foundError(int which); 
-        static constexpr bool is_severe[9] = {true, true, true, true, false, false, true, true, true};
         static const int READ_ERROR = 0;
         static const int WRITE_ERROR = 1;
         static const int CANT_DECODE_SYMBOL = 2;
@@ -15,5 +13,8 @@ class ErrorHandler {
         static const int NO_SYMBOLS_IN_FILE = 5;
         static const int FILE_NOT_FOUND_OR_EMPTY = 6;
         static const int TRIE_ERROR = 7;
-        static const int CANT_READ_CODES = 8;
+        static const int CANT_READ_CODES = 8;   
+        static const int CONFUSING_ARGUMENTS = 9;
+        static const int WRONG_ARGUMENTS = 10;
+        static const int NO_ARGUMENTS = 11;
 };
--- main.cpp
+++ main.cpp
@@ -1,16 +1,69 @@
+#include <cstring>
 #include <iostream>
+#include <unordered_map>
+#include "error_handler/error_handler.h"
 #include "archiver/archiver.h"
 #include "writer/writer.h"
 
 using namespace std;
 
-int main() {
-    Archiver archiver("a");
-    Writer writer("a_compressed3");
-    archiver.CompressTo(writer, true);
-    
-    Archiver archiver2("a");
-    archiver2.CompressTo(writer, true);
-    writer.Finish();
+const string COMPRESS_ARG = "-c";
+const string DECOMPRESS_ARG = "-d";
+const string HELP_ARG = "-h";
+const string HELP_TEXT = "Compressing files [file1, file2, ...] to file with name 'archive_name': 'archiver -c archive_name file1 [file2 ...]'.\n"
+                         "Decompressing file archive_name to current folder: 'archiver -d archive_name'.";
+void ShowHelpAndExit() {
+    std::cout << HELP_TEXT << std::endl;
+    exit(EXIT_SUCCESS);
+}
+
+pair<bool, vector<string> > ParseArguments(int argc, char* argv[]) {
+    if (argc == 1) {
+        ErrorHandler::foundError(ErrorHandler::NO_ARGUMENTS);
+        return {false, vector<string>()};
+    } else {
+        vector<string> result;
+        bool has_compress, has_decompress;
+        for (int i = 1; i < argc; ++i) {
+            if (strcmp(argv[i], "-c") == 0) {
+                has_compress = true;
+            } 
+            if (strcmp(argv[i], "-d") == 0) {
+                has_decompress = true;
+            }
+            if (strcmp(argv[i], "-h") == 0) {
+                ShowHelpAndExit();
+            }
+            result.push_back(argv[i]);
+        }
+        if (has_compress && has_decompress) {
+            ErrorHandler::foundError(ErrorHandler::CONFUSING_ARGUMENTS);
+        }
+        if (has_compress && result.size() < 3) {
+            ErrorHandler::foundError(ErrorHandler::WRONG_ARGUMENTS);
+        }
+        if (has_decompress && result.size() < 2) {
+            ErrorHandler::foundError(ErrorHandler::WRONG_ARGUMENTS);
+        }
+        return {has_compress, result};
+    }
+}
+
+int main(int argc, char* argv[]) {
+    pair<bool, vector<string> > p = ParseArguments(argc, argv);
+    bool is_compress = p.first;
+    vector<string> args = p.second;
+    if (is_compress) {
+        Writer writer(args[1]);
+        for (size_t i = 2; i < args.size(); ++i) {
+            std::cout << args[i] << std::endl;
+            Archiver archiver(args[i]);
+            archiver.CompressTo(writer, (i + 1 == args.size()));
+        }       
+        writer.Finish();
+    } else {
+        Archiver archiver(args[1]);
+        archiver.DecompressTo(".");
+    }
     return 0;
 }
