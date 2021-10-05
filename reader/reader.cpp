15a388ae65933cdb7e200442caf982c75bbc42f4
Ihor Chovpan <cigo1234r@gmail.com>
2021-10-05 19:36:56 +0300

    working on decompressing, added error handler, changed Reader API

--- reader/reader.cpp
+++ reader/reader.cpp
@@ -8,23 +8,51 @@ Reader::Reader(const std::string& file_path) : file_(file_path, std::ios::binary
     file_.seekg(0);
 }
 
-unsigned char Reader::read_next_byte() {
+bool Reader::IsEOF() {
+    return (bytes_read_ >= file_size_ && buf_pos_ == 8);
+}
+
+uint8_t Reader::Read8() {
+    uint8_t result;
+    for (size_t i = 0; i < 8; ++i) {
+        result *= 2;
+        result += Read1();
+    }
+    return result;
+}
+
+uint16_t Reader::Read9() {
+    uint16_t result;
+    for (size_t i = 0; i < 9; ++i) {
+        result *= 2;
+        result += Read1();
+    }
+    return result;
+}
+
+unsigned char Reader::ReadByte() {
     char buffer;
 
     file_.read(&buffer, 1);
     ++bytes_read_;
-
     return buffer;
 }
 
-bool Reader::has_next_byte() const {
-    return bytes_read_ < file_size_;
+bool Reader::Read1() {
+    if (buf_pos_ < 8) {
+        return (buf_ >> (7 - buf_pos_++)) & 1;
+    } else if (bytes_read_ < file_size_) {
+        buf_ =  ReadByte();
+        buf_pos_ = 1;
+        return (buf_ >> 7) & 1;
+    } else
+        return false;
 }
 
-std::string Reader::file_name() const {
+std::string Reader::FileName() const {
     return file_path_.substr(file_path_.find_last_of("/\\") + 1);
 }
 
-size_t Reader::file_size() {
+size_t Reader::FileSize() {
     return file_size_;
 }
--- reader/reader.h
+++ reader/reader.h
@@ -4,15 +4,20 @@
 class Reader {
     public:
         Reader(const std::string& file_path);
-
-        unsigned char read_next_byte();
-        bool has_next_byte() const;
-        std::string file_name() const;
-        size_t file_size();
+        
+        bool Read1();
+        uint16_t Read9();
+        uint8_t Read8();
+        std::string FileName() const;
+        size_t FileSize();        
+        bool IsEOF();
 
     private:
         std::ifstream file_;
         std::string file_path_;
         size_t file_size_ = 0;
         size_t bytes_read_ = 0;
+        uint8_t buf_;
+        size_t buf_pos_ = 8; 
+        unsigned char ReadByte();
 };
